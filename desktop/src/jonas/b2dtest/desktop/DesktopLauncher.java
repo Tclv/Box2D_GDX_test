9216eba324c54bed9edb97f8ae9ac26ade7de921
Jonas Carpay <jonascarpay@gmail.com>
2015-12-24 14:26:15 +0100

    Initialized project

---
+++ desktop/src/jonas/b2dtest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package jonas.b2dtest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import jonas.b2dtest.Bmain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bmain(), config);
+	}
+}
