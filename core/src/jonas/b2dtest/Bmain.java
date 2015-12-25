3f89e7bb35053d8001988ba21c0a6c02f0ab65ae
Jonas Carpay <jonascarpay@gmail.com>
2015-12-25 15:52:08 +0100

    Debugrenderer, more objects

--- core/src/jonas/b2dtest/Bmain.java
+++ core/src/jonas/b2dtest/Bmain.java
@@ -3,28 +3,35 @@ package jonas.b2dtest;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 
 public class Bmain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	PhysicsWorld physicsWorld;
+    OrthographicCamera camera;
+    private Box2DDebugRenderer debugRenderer;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		physicsWorld = new PhysicsWorld();
+        camera = new OrthographicCamera(80,50);
+        camera.position.set(0,20,0);
+        debugRenderer = new Box2DDebugRenderer();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
         physicsWorld.update(1f/60f);
+        debugRenderer.render(physicsWorld.b2world, camera.combined);
 	}
 }
--- core/src/jonas/b2dtest/PhysicsWorld.java
+++ core/src/jonas/b2dtest/PhysicsWorld.java
@@ -1,5 +1,7 @@
 package jonas.b2dtest;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
@@ -31,32 +33,33 @@ public class PhysicsWorld {
         groundBody.createFixture(groundShape, 0); // Zero density, as static bodies have a mass of 0 by definition. A FixtureDef builder exists, but for this static body this method suffices.
     }
 
-    /**
-     * For the construction of a dynamic body we need the following elements:
-     * BodyDef: defines non-physical properties of the body in the simulation, used as builder for World.createBody
-     * Body: Reference to the body in the world
-     * PolygonShape: Gives shape to the body
-     * FixtureDef: Defines physical properties of the body, used as builder for createFixture.
-     */
     private void initBox() {
+        final int NUM_BOXES = 20;
         BodyDef boxBodyDef = new BodyDef();
         boxBodyDef.type = BodyDef.BodyType.DynamicBody;
-        boxBodyDef.position.set(new Vector2(0f, 4f));
-        boxBody = b2world.createBody(boxBodyDef);
         PolygonShape boxShape = new PolygonShape();
         boxShape.setAsBox(1f, 1f);
         FixtureDef boxFixtureDef = new FixtureDef();
         boxFixtureDef.shape = boxShape;
         boxFixtureDef.density = 1f;
         boxFixtureDef.friction = 0.3f;
-        boxBody.createFixture(boxFixtureDef);
+        for (int i = 0; i < NUM_BOXES; i++) {
+            boxBodyDef.fixedRotation = false;
+            boxBodyDef.position.set(MathUtils.random(-20, -15), MathUtils.random(0,50));
+            boxBody = b2world.createBody(boxBodyDef);
+            boxBody.createFixture(boxFixtureDef);
+        }
+        for (int i = 0; i < NUM_BOXES; i++) {
+            boxBodyDef.fixedRotation = true;
+            boxBodyDef.position.set(MathUtils.random(15, 20), MathUtils.random(0,50));
+            b2world.createBody(boxBodyDef).createFixture(boxFixtureDef);
+        }
     }
 
     public void update(float deltaTime) {
         // Properties of the simulation, values recommended by the manual. Higher numbers entail more accuracy at the cost of performance. These bear no relation to the time step.
-        final int velocityIterations = 8;
+        final int velocityIterations = 6;
         final int positionIterations = 2;
         b2world.step(deltaTime, velocityIterations, positionIterations);
-        System.out.println(String.format("x: %1.2f, y: %1.2f, dx: %1.2f, dy: %1.2f", boxBody.getPosition().x, boxBody.getPosition().y, boxBody.getLinearVelocity().x, boxBody.getLinearVelocity().y));
     }
 }
